f87715bab577187191dbcb3b3ca700f7317d890d
Sidharth Mittal <ssidharthmittal@gmail.com>
2020-10-05 14:51:34 +0530

    add hystrix dashboard and spring boot actuator

--- movie-catalog-service/src/main/java/io/github/sidharthmittal/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/github/sidharthmittal/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,8 @@ import java.beans.BeanProperty;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableEurekaClient
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 
 	@Bean
